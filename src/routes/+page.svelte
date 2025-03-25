3a66208b4bb8f72ba7edf4f628533ee598adef83
Francesca Suba <subaap@mail.uc.edu>
2025-03-25 14:40:17 -0400

    install tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="bg-cyan-800"><h1 class="text-4xl md:text-5xl font-bold mb-8">Projects</h1></div>
